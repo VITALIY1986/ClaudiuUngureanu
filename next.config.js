ee27142b9d5a28cf9aa123e2a7408eb462861591
VITALIY1986 <staslego1986@gmail.com>
2023-11-01 14:28:27 +0200

    asdasd

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
-
-module.exports = withPWA({
-  // next.js config
-})// next.config.js
 
 
 
